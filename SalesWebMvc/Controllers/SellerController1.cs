e5c768809d889105c35e34bbdd5426b7517b2680
Pedro Henrique <phsferreira06@gmail.com>
2025-05-20 16:58:25 -0300

    SellerController

---
+++ SalesWebMvc/Controllers/SellerController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellerController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
