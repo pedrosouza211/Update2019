ce4c7e4e8f2794a43330c2790329f9c98e11ab01
Pedro Henrique <phsferreira06@gmail.com>
2025-05-20 17:32:52 -0300

    SellerService and badic FindAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -32,6 +33,7 @@ namespace SalesWebMvc
           );
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
 
         }
 
